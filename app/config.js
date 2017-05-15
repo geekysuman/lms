aeeec9e7f9a68f5c78f7d10ebcad133748d337d0
geekysuman <suman.sarkar@imaginea.com>
2017-05-15 16:07:56 +0530

    changes in gitignore and config file

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 // Transaction id should be replaced in the file to connect ot the backend
 const Contract = {
-  id: '0x20090b88b455dc3dc497874f618f2b2124aa6c6f'
+  id: '0x95b2f7f949f7b98d9306db39fd7ac120d51c1bca'
 }
 
 export default Contract
